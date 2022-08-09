b25d005e7ee1339b76e03a26cf90fe1083aada8f
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2022-08-09 18:29:33 -0300

    Aplication shows if the number is a multiple of 2

---
+++ multiples2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main ()
+{
+    int num, resto;
+    cout << "Informe um número qualquer:";
+    cin >> num;
+    resto=num%2;
+    if(resto==0)
+    {
+        cout << "Este número é múltiplo de 2!";
+    }
+}
